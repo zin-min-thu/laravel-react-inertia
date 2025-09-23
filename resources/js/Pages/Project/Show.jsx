987b61f547491a6aaee0c76c86bfc14e410904d3
KoZin <zinminthu.dev@gmail.com>
2025-09-23 23:23:59 +0700

    add project detail page with related tasks

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -1,7 +1,9 @@
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from '@/constants.jsx';
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
+import TasksTable from '../Task/TasksTable';
 
-export default function Show({ auth, project }) {
+export default function Show({ auth, project, tasks, queryParms = null  }) {
     return (
         <AuthenticatedLayout
                     header={
@@ -16,14 +18,59 @@ export default function Show({ auth, project }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            Show Project Detail
+                            <div className="grid grid-cols-1">
+                                <div className="...">
+                                    <img className="w-full h-64 object-cover" src={project.image_path}></img>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-2 gap-4 mt-4">
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Project ID</h2>
+                                    <p>{project.id}</p>
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Due Date</h2>
+                                    <p>{project.due_date}</p>
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Project Name</h2>
+                                    <p>{project.name}</p>
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Created Date</h2>
+                                    <p>{project.created_at}</p>
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Project Status</h2>
+                                    <span className={`px-2 py-1 rounded text-white ${PROJECT_STATUS_CLASS_MAP[project.status]}`}>
+                                        {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                    </span>
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Updated By</h2>
+                                    <p>{project.created_at}</p>
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Created By</h2>
+                                    <p>{project.createdBy.name}</p>
+                                </div>
+                                <div className="...">
+                                </div>
+                                <div className="...">
+                                    <h2 className="text-xl font-semibold leading-tight text-gray-800">Project Description</h2>
+                                    <p>{project.description}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 mt-4">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            Show Tasks
+                            <TasksTable
+                                tasks={tasks}
+                                queryParms={queryParms}
+                            />
                         </div>
                     </div>
                 </div>
--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -1,48 +1,10 @@
-import Pagination from '@/Components/Pagination';
-import SelectInput from '@/Components/SelectInput';
-import TextInput from '@/Components/TextInput';
-import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from '@/constants.jsx';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, router } from '@inertiajs/react';
 
-import TableHeading from '@/Components/TableHeading';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+import TasksTable from './TasksTable';
 
 export default function Index({ auth , tasks, queryParms = null }) {
-
-    if(!queryParms) {
-        queryParms = {};
-    }
-
-    const searchFieldChanged = (name, value) => {
-        if(value) {
-            queryParms[name] = value;
-        } else {
-            delete queryParms[name];
-        }
-
-        router.get(route('task.index', queryParms));
-    }
-
-    const onKeyPress = (name, e) => {
-
-        if(e.key !== 'Enter') {
-            return;
-        }
-
-        searchFieldChanged(name, e.target.value);
-    }
-
-    const sortChanged = (name) => {
-        if(queryParms.sort_field === name) {
-            queryParms.sort_direction = queryParms.sort_direction === 'asc' ? 'desc' : 'asc';
-        } else {
-            queryParms.sort_field = name;
-            queryParms.sort_direction = 'asc';
-        }
-
-        router.get(route('task.index', queryParms));
-    }
-
 
     return (
         <AuthenticatedLayout
@@ -58,117 +20,12 @@ export default function Index({ auth , tasks, queryParms = null }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
+                        
+                            <TasksTable
+                                tasks={tasks}
+                                queryParms={queryParms}
+                            />
                             
-                            {/* Table Responsive wrapper */}
-                            <div className="overflow-x-auto">
-                                <table className="min-w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    <thead className="bg-gray-50">
-                                        <tr className="divide-x divide-gray-200">
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500"></th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500"></th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500">
-                                                <TextInput
-                                                    defaultValue={queryParms.name}
-                                                    className="w-full"
-                                                    placeholder="Task Name"
-                                                    onBlur={e => searchFieldChanged('name', e.target.value)}
-                                                    onKeyPress={e => onKeyPress('name', e)}
-                                                />
-                                            </th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500">
-                                                <SelectInput
-                                                    defaultValue={queryParms.status}
-                                                    className="w-full"
-                                                    onChange={e => searchFieldChanged('status', e.target.value)}
-                                                >
-                                                    <option value="">Select Status</option>
-                                                    <option value="pending">Pending</option>
-                                                    <option value="in_progress">In Progress</option>
-                                                    <option value="completed">Completed</option>
-                                                </SelectInput>
-                                            </th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500"></th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500"></th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500"></th>
-                                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider text-gray-500"></th>
-                                        </tr>
-                                        <tr className="divide-x divide-gray-200">
-                                            <TableHeading
-                                                name="id"
-                                                sort_field={queryParms.sort_field}
-                                                sort_direction={queryParms.sort_direction}
-                                                sortChanged={sortChanged}
-                                            >
-                                                ID
-                                            </TableHeading>
-                                            <TableHeading sortable = {false}>
-                                                Image
-                                            </TableHeading>
-                                            <TableHeading
-                                                name="name"
-                                                sort_field={queryParms.sort_field}
-                                                sort_direction={queryParms.sort_direction}
-                                                sortChanged={sortChanged}
-                                            >
-                                                Name
-                                            </TableHeading>
-                                            <TableHeading
-                                                name="status"
-                                                sort_field={queryParms.sort_field}
-                                                sort_direction={queryParms.sort_direction}
-                                                sortChanged={sortChanged}
-                                            >
-                                                Status
-                                            </TableHeading>
-                                            <TableHeading
-                                                name="created_at"
-                                                sort_field={queryParms.sort_field}
-                                                sort_direction={queryParms.sort_direction}
-                                                sortChanged={sortChanged}
-                                            >
-                                                Created Date
-                                            </TableHeading>
-                                            <TableHeading
-                                                name="due_date"
-                                                sort_field={queryParms.sort_field}
-                                                sort_direction={queryParms.sort_direction}
-                                                sortChanged={sortChanged}
-                                            >
-                                                Due Date
-                                            </TableHeading>
-                                            <th className="px-3 py-3 text-xs font-medium uppercase tracking-wider text-gray-500">Created By</th>
-                                            <th className="px-3 py-3 text-xs font-medium uppercase tracking-wider text-gray-500">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {tasks.data.map((task) => (
-                                            <tr key={task.id} className="divide-x divide-gray-200 hover:bg-gray-50">
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{task.id}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    <img className="h-10 w-10 rounded-sm" src={task.image_path} alt={task.name} />
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{task.name}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                                    <span className={`px-2 py-1 rounded text-white ${PROJECT_STATUS_CLASS_MAP[task.status]}`}>
-                                                        {PROJECT_STATUS_TEXT_MAP[task.status]}
-                                                    </span>
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{task.created_at}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{task.due_date}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{task.createdBy.name}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 flex space-x-2">
-                                                    <a href={route('task.edit', task.id)} className="text-indigo-600 hover:text-indigo-900">View</a>
-                                                    <a href={route('task.destroy', task.id)} className="text-red-600 hover:text-red-800">Edit</a>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-
-                            <div className="mt-4">
-                                <Pagination links={tasks.meta.links} />
-                            </div>
                         </div>
                     </div>
                 </div>
